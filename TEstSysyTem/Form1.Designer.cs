7446ba4dfb9b5d4f5a6463d94f5aa009e5beafb9
KhangChina <40732804+KhangChina@users.noreply.github.com>
2020-04-01 14:25:36 +0700

    1_4_20
    ok con de

---
+++ TEstSysyTem/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TEstSysyTem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.s1 = new System.Windows.Forms.TextBox();
+            this.s2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(470, 199);
+            this.listBox1.TabIndex = 1;
+            // 
+            // s1
+            // 
+            this.s1.Location = new System.Drawing.Point(93, 5);
+            this.s1.Name = "s1";
+            this.s1.Size = new System.Drawing.Size(166, 20);
+            this.s1.TabIndex = 2;
+            // 
+            // s2
+            // 
+            this.s2.Location = new System.Drawing.Point(265, 5);
+            this.s2.Name = "s2";
+            this.s2.Size = new System.Drawing.Size(211, 20);
+            this.s2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 239);
+            this.Controls.Add(this.s2);
+            this.Controls.Add(this.s1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox s1;
+        private System.Windows.Forms.TextBox s2;
+    }
+}
+
---
+++ TEstSysyTem/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEstSysyTem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool match = Regex.IsMatch(s1.Text, s2.Text);//So Sánh kí tự bất kì trong s2 có trong s1
+            listBox1.Items.Add(match.ToString());
+        }
+        
+    }
+}
